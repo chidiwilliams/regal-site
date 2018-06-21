12a79809b637c3a219f6ee978dd79aa37db92479
Williams Chidi <williamschidi1@gmail.com>
2018-06-21 19:15:02 +0100

    Added fonts bootstrap home text and design

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
-var nunjucksRender = require('gulp-nunjucks-render');
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const sass = require('gulp-sass');
 
-gulp.task('build', function () {
+gulp.task('build', function() {
   // Gets .html and .nunjucks files in pages
   return gulp
     .src('views/*.@(html|njk)')
@@ -13,4 +14,12 @@ gulp.task('build', function () {
     .pipe(gulp.dest('out'));
 });
 
+gulp.task('sass', function() {
+  return gulp
+    .src('assets/scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('out/public/css'));
+});
+
+gulp.start('sass');
 gulp.start('build');
