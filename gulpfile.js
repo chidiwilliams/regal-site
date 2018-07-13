bea2c66783a0802b4a743a480fadcb055326a240
Williams Chidi <williamschidi1@gmail.com>
2018-07-13 09:04:49 +0100

    Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const sass = require('gulp-sass');
+
+gulp.task('build', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp
+    .src('views/*.@(html|njk)')
+    .pipe(
+      nunjucksRender({
+        path: ['views'],
+      })
+    )
+    .pipe(gulp.dest('out'));
+});
+
+gulp.task('sass', function() {
+  return gulp
+    .src('assets/scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('out/assets/css'));
+});
+
+module.exports = gulp;
